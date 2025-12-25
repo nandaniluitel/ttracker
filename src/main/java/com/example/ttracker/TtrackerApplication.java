6838f6728bce6c1f3317c259cdeb2bff62874a66
nandaniluitel <nandanicode@gmail.com>
2025-12-26 05:30:14 +0545

    Initial commit: Spring Boot project

---
+++ src/main/java/com/example/ttracker/TtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ttracker/TtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
