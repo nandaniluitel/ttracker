028e8ac957436e95fcbd5133f1d1cf0dd84a4577
ishankhadka <ishankhadka44@gmail.com>
2026-02-05 21:55:11 +0100

    feat: added validation and ut for create epic commands

--- src/main/java/com/example/ttracker/application/port/in/epics/CreateEpicCommand.java
+++ src/main/java/com/example/ttracker/application/port/in/epics/CreateEpicCommand.java
@@ -11,5 +11,19 @@ public record CreateEpicCommand(
 
 ) {
 
+     public CreateEpicCommand {
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("Epic title must not be null or blank");
+        }
+        if (description == null || description.isBlank()) {
+            throw new IllegalArgumentException("Epic description must not be null or blank");
+        }
+        if (priority == null) {
+            throw new IllegalArgumentException("Epic priority must not be null");
+        }
+        if (assigneeUserId == null) {
+            throw new IllegalArgumentException("Assignee user ID can't be null");
+        }
+     }
 
 }
